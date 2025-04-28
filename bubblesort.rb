9661d0614456d014682e83e7c29f230b67941b0a
Nizamuddin Khan <nizam12khan@gmail.com>
2025-04-28 23:31:06 +0530

    bubble sorting done

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,15 +1,15 @@
 def bubblesort(a)
+    n=a.length-1
     flag =true
     while flag
         flag = false
-         for i in 0..a.length-2
+        for i in 0..n-1
             if a[i+1] <a[i]
-                temp=a[i]
-                a[i]=a[i+1]
-                a[i+1]=temp
+                a[i],a[i+1] =a[i+1],a[i]
                 flag = true
             end
         end
+        n -= 1
     end
     a
 end
